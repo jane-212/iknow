4444191bff5045f55069bc158b6cffee88c636f5
jane <jjt1525@outlook.com>
2023-09-19 15:00:44 +0800

    add csgo cron from 12 to 12,17

--- src/main.rs
+++ src/main.rs
@@ -146,11 +146,11 @@ async fn run(
     let csgo = Csgo::new(username, password, from, reply_to, to).context("init csgo failed")?;
     #[cfg(debug_assertions)]
     let manager = Manager::new()
-        .add("*/10 * * * * *", "csgo", Box::new(csgo))
+        .add("*/5 * * * * ?", "csgo", Box::new(csgo))
         .context("add cron job failed")?;
     #[cfg(not(debug_assertions))]
     let manager = Manager::new()
-        .add("0 0 12 * * *", "csgo", Box::new(csgo))
+        .add("0 0 12,17 * * ?", "csgo", Box::new(csgo))
         .context("add cron job failed")?;
     tokio::spawn(async move {
         manager.start().await;
